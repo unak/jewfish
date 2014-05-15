8c3fdfbbd43e9121160d1fd8427b1a9c9e567a60
U.Nakamura <usa@garbagecollect.jp>
2014-05-16 05:01:09 +0900

    fixed wrong private.

--- lib/jewfish/command.rb
+++ lib/jewfish/command.rb
@@ -4,69 +4,71 @@ module Jewfish
       run: :start
     }
 
-    def self.run(*args)
-      cmd = args.shift || 'help'
-      cmd = @@synonims[cmd.to_sym].to_s if @@synonims[cmd.to_sym]
-      lib = File.join(File.dirname(__FILE__), 'commands', cmd + '.rb')
-      raise "unknown subcommand: #{cmd}" unless File.exist?(lib)
-      require lib
-      mod = Jewfish.const_get(cmd.capitalize)
-      if args.size < mod.req || (!mod.rest && args.size > mod.req + mod.opt)
-        $stderr.puts "jewfish: not enough or too many parameters."
-        $stderr.puts
-        mod.show_usage($stderr)
-        exit 1
+    class << self
+      def run(*args)
+        cmd = args.shift || 'help'
+        cmd = @@synonims[cmd.to_sym].to_s if @@synonims[cmd.to_sym]
+        lib = File.join(File.dirname(__FILE__), 'commands', cmd + '.rb')
+        raise "unknown subcommand: #{cmd}" unless File.exist?(lib)
+        require lib
+        mod = Jewfish.const_get(cmd.capitalize)
+        if args.size < mod.req || (!mod.rest && args.size > mod.req + mod.opt)
+          $stderr.puts "jewfish: not enough or too many parameters."
+          $stderr.puts
+          mod.show_usage($stderr)
+          exit 1
+        end
+
+        mod.new(*args)
       end
 
-      mod.new(*args)
-    end
+      def show_desc(io = $stdout)
+        io.puts "#{synonims}: #{@desc}"
+      end
 
-    def self.show_desc(io = $stdout)
-      io.puts "#{synonims}: #{@desc}"
-    end
+      def show_longdesc(io = $stdout)
+        return unless @longdesc
+        io.puts
+        io.puts @longdesc
+      end
 
-    def self.show_longdesc(io = $stdout)
-      return unless @longdesc
-      io.puts
-      io.puts @longdesc
-    end
+      def show_usage(io = $stdout)
+        io.puts "Usage: jewfish #{@usage}"
+      end
 
-    def self.show_usage(io = $stdout)
-      io.puts "Usage: jewfish #{@usage}"
-    end
+      def req
+        reqs = instance_method(:initialize).parameters.find{|e| e.first == :req}
+        reqs ? reqs.size - 1 : 0
+      end
 
-    def self.req
-      reqs = instance_method(:initialize).parameters.find{|e| e.first == :req}
-      reqs ? reqs.size - 1 : 0
-    end
+      def opt
+        opts = instance_method(:initialize).parameters.find{|e| e.first == :opt}
+        opts ? opts.size - 1 : 0
+      end
 
-    def self.opt
-      opts = instance_method(:initialize).parameters.find{|e| e.first == :opt}
-      opts ? opts.size - 1 : 0
-    end
+      def rest
+        instance_method(:initialize).parameters.find{|e| e.first == :rest}
+      end
 
-    def self.rest
-      instance_method(:initialize).parameters.find{|e| e.first == :rest}
-    end
+      def synonims
+        name = self.name.to_s.sub(/.*::/, '').downcase
+        syns = @@synonims.select{|k, v| v.to_s == name}.map{|k, v| k.to_s}
+        name << " (#{syns.join(', ')})" unless syns.empty?
+        name
+      end
 
-    def self.synonims
-      name = self.name.to_s.sub(/.*::/, '').downcase
-      syns = @@synonims.select{|k, v| v.to_s == name}.map{|k, v| k.to_s}
-      name << " (#{syns.join(', ')})" unless syns.empty?
-      name
-    end
+      private
+      def desc(str)
+        @desc = str
+      end
 
-    private
-    def self.desc(str)
-      @desc = str
-    end
+      def longdesc(str)
+        @longdesc = str
+      end
 
-    def self.longdesc(str)
-      @longdesc = str
-    end
-
-    def self.usage(str)
-      @usage = str
+      def usage(str)
+        @usage = str
+      end
     end
 
     def parse_options(args)
