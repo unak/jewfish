3cbea006cbb3af3127e03e9dcb0e26c90874cce0
U.Nakamura <usa@garbagecollect.jp>
2014-11-08 02:45:15 +0900

    Remove debug code.

--- lib/jewfish/commands/start.rb
+++ lib/jewfish/commands/start.rb
@@ -59,7 +59,6 @@ options:
         src = File.join(srcdir, path)
         found = false
         Dir.glob([File.join(srcdir, path.sub(%r'\.[^/]*\z', '') + '.*'), File.join(srcdir, File.dirname(path), '_posts', File.basename(path, '.*') + '.*'), File.join(srcdir, File.dirname(path.sub(%r'/\d{4}/\d{2}/\d{2}/[^/]+/', '/')), '_posts', File.basename(path.sub(%r'/(\d{4})/(\d{2})/(\d{2})/([^/]+)/index', '/\\1-\\2-\\3-\\4'), '.*') + '.*')]) do |file|
-          p [file, src, Format.output_filename(file)]
           if Format.output_filename(file) == src
             res.body = Page.convert(file, path)
             found = true
